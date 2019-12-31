6460de2939f0da77d09408ebf8c980b9b07de14a
seferinus <47113244+seferinus@users.noreply.github.com>
2019-12-31 10:15:46 +0100

    Update thefunc.go

--- thefunc.go
+++ thefunc.go
@@ -19,7 +19,7 @@ func HelloWorld(w http.ResponseWriter, r *http.Request) {
     return
   }
   if d.Message == "" {
-    fmt.Fprint(w, "Hello World!")
+    fmt.Fprint(w, "teh vest fysh")
     return
   }
   fmt.Fprint(w, html.EscapeString(d.Message))
